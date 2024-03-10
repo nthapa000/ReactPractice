77bffcf9691f8a5bfb691f652515c9182965ae5a
nthapa000 <nishantthapa0000@gmail.com>
2024-03-10 17:06:55 +0530

    Method2

--- week-6-2-practice/src/App.jsx
+++ week-6-2-practice/src/App.jsx
@@ -1,12 +1,16 @@
-import {  useState } from "react";
+import {  useEffect, useState } from "react";
 export default function App(){
   const [counter,setCounter]= useState(0);
   const [inputValue,setInputValue] = useState(1);
+  const [finalValue,setFinalValue] = useState(0);
 
-  let count =0;
+  useEffect(()=>{
+    let count =0;
   for(let i=1; i<= inputValue;i++){
     count = count + i;
   }
+  setFinalValue(count);
+  },[inputValue])
 
   return <div>
     <input onChange={function(e){
@@ -14,7 +18,7 @@ export default function App(){
     }} placeholder="Find sum from 1 to n" />
     
     <br />
-    Sum from 1 to {inputValue} is {count}
+    Sum from 1 to {inputValue} is {finalValue}
     <br />
     <button onClick={()=>{
       setCounter(counter+1);
