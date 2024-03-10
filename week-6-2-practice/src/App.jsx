81283bc3b3ab52937aac9f2a7bae6ce3ade5719d
nthapa000 <nishantthapa0000@gmail.com>
2024-03-10 15:59:26 +0530

    Displaying todo by Clicking on button

--- week-6-2-practice/src/App.jsx
+++ week-6-2-practice/src/App.jsx
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+// cleaner syntax to do the fetch call
 
 export default function App(){
+  const [currentId,setCurrentId] = useState(1)
+
   return <div>
-    <Todo id={5}/>
+    <button onClick={function(){
+      setCurrentId(1)
+    }}>1</button>
+    <button onClick={function(){
+      setCurrentId(2)
+    }}>2</button>
+    <button onClick={function(){
+      setCurrentId(3)
+    }}>3</button>
+    <button onClick={function(){
+      setCurrentId(4)
+    }}>4</button>
+
+    <Todo id={currentId}/>
   </div>
 }
 
 function Todo({id}){
+  
   const [todo,setTodo] = useState({});
 
   useEffect(()=>{
@@ -16,9 +33,10 @@ function Todo({id}){
       setTodo(response.data.todo)
       // what our backend returns
     })
-  },[])
+  },[id])
 
   return <div>
+    Id:{id}
     <h1>{todo.title}</h1>
     <h4>{todo.description}</h4>
   </div>
